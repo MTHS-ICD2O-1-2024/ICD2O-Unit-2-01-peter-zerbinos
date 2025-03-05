88bbd0442a2c6136db7e93f30a92fd5f56736682
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-03-05 18:24:56 +0000

    edited the code to make it work

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@
 // Created on: Feb 2025
 // This file contains the JS functions for index.html
 
-function myButtonClicked () {
-  alert('Hello, World!')
+function myButtonClicked() {
+  document.getElementById("answer").innerHTML = "Hello, World!"
 }
